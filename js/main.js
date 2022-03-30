16a4a1e1737ac24b85c3b95de9f2d3bc1121b5af
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-03-30 16:19:17 +0500

    js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,36 @@
-$('.header__burgir .btn2').click(function() {
-    $('.header__navbar').toggleClass('active');
-    $('.header__search').removeClass('active');
-});
-$('.header__burgir .btn').click(function() {
-    $('.header__search').toggleClass('active');
-    $('.header__navbar').removeClass('active');
-});
+function classToggleRemove(className, classNameToggle, classNameRemove) {
+    $(`${className}`).click(function() {
+        $(`${classNameToggle}`).toggleClass('active');
+        $(`${classNameRemove}`).removeClass('active');
+    }); 
+};
+classToggleRemove('.header__burgir .btn2', '.header__navbar', '.header__search');
+classToggleRemove('.header__burgir .btn', '.header__search', '.header__navbar');
+
 $('.main').click(function() {
     $('.header__navbar').removeClass('active');
     $('.header__search').removeClass('active');
 });
 
-$('.regulations__left .item').click(function() {
-    $(this).toggleClass('active')
-});
-$('.form_black__accordion').click(function() {
-    $(this).toggleClass('active');
-});
-$('.autonomous_institutions__aside .item h5').click(function() {
-    $(this).toggleClass('active');
-});
-$('.autonomous_institutions__aside h6').click(function() {
-    $(this).toggleClass('active');
-});
-$('.autonomous_institutions__block').click(function() {
-    $(this).toggleClass('active');
-});
-$('.kbk_2__accordion').click(function() {
-    $(this).toggleClass('active');
-});
+function classToggle(className) {
+    $(`${className}`).click(function() {
+        $(this).toggleClass('active');
+    }); 
+};
+classToggle('.regulations__left .item');
+classToggle('.form_black__accordion');
+classToggle('.autonomous_institutions__aside .item h5');
+classToggle('.autonomous_institutions__aside h6');
+classToggle('.autonomous_institutions__block');
+classToggle('.kbk_2__accordion');
+
 $('.header__connect').click(function() {
     $('.not_enough_rights').addClass('active');
 });
 $('.modal__close').click(function() {
     $('.modal').removeClass('active');
 });
+
 $('.title_h2__open_filter').click(function() {
     if($('.autonomous_institutions__aside')) {
         $('.bg').addClass('active');
