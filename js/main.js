334417421c3e93a41efadf08849250782ded26f8
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-03-30 21:31:45 +0500

    js min

--- js/main.js
+++ js/main.js
@@ -4,49 +4,40 @@ function classToggleRemove(className, classNameToggle, classNameRemove) {
         $(`${classNameRemove}`).removeClass('active');
     }); 
 };
-classToggleRemove('.header__burgir .btn2', '.header__navbar', '.header__search');
-classToggleRemove('.header__burgir .btn', '.header__search', '.header__navbar');
-
-$('.main').click(function() {
-    $('.header__navbar').removeClass('active');
-    $('.header__search').removeClass('active');
+classToggleRemove('#header__burgir_btn2', '#header__navbar', '#header__search');
+classToggleRemove('#header__burgir_btn', '#header__search', '#header__navbar');
+$('#main').click(function() {
+    $('#header__navbar').removeClass('active');
+    $('#header__search').removeClass('active');
 });
 
-function classToggle(className) {
-    $(`${className}`).click(function() {
-        $(this).toggleClass('active');
-    }); 
-};
-classToggle('.regulations__left .item');
-classToggle('.form_black__accordion');
-classToggle('.autonomous_institutions__aside .item h5');
-classToggle('.autonomous_institutions__aside h6');
-classToggle('.autonomous_institutions__block');
-classToggle('.kbk_2__accordion');
+$('.classToggle').click(function() {
+    $(this).toggleClass('active');
+});
 
-$('.header__connect').click(function() {
-    $('.not_enough_rights').addClass('active');
+$('#header__connect').click(function() {
+    $('#not_enough_rights').addClass('active');
 });
 $('.modal__close').click(function() {
     $('.modal').removeClass('active');
 });
 
-$('.title_h2__open_filter').click(function() {
-    if($('.autonomous_institutions__aside')) {
-        $('.bg').addClass('active');
-        $('.autonomous_institutions__aside').addClass('active');
+$('#title_h2__open_filter').click(function() {
+    if($('#autonomous_institutions__aside')) {
+        $('#bg').addClass('active');
+        $('#autonomous_institutions__aside').addClass('active');
     }
-    if($('.kbk_2__aside')) {
-        $('.bg').addClass('active');
-        $('.kbk_2__aside').addClass('active');
+    if($('#kbk_2__aside')) {
+        $('#bg').addClass('active');
+        $('#kbk_2__aside').addClass('active');
     }
 });
 $('.filter_close').click(function() {
-    $('.bg').removeClass('active');
-    if($('.autonomous_institutions__aside')) {
-        $('.autonomous_institutions__aside').removeClass('active');
+    $('#bg').removeClass('active');
+    if($('#autonomous_institutions__aside')) {
+        $('#autonomous_institutions__aside').removeClass('active');
     }
-    if($('.kbk_2__aside')) {
-        $('.kbk_2__aside').removeClass('active');
+    if($('#kbk_2__aside')) {
+        $('#kbk_2__aside').removeClass('active');
     }
 });
